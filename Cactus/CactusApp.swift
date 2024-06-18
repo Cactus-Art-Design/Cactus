11f2ca308d96111c1528dca62503a5da5e5942b1
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2024-06-18 18:58:48 -0400

    initial commit

---
+++ Cactus/CactusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CactusApp.swift
+//  Cactus
+//
+//  Created by Brian Masse on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CactusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
